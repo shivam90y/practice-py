5630e3da9ed54b3eb0f03cd29fce547452a71b07
Shivam Yadav <shivamy@coindelta.com>
2019-01-04 18:59:50 +0530

    linked list

--- infinite_loop.py
+++ infinite_loop.py
@@ -1,4 +1,4 @@
-a=3
+a=1
 while(a>0):
  print(a)
-a=a-1
+#  a=a-1
--- nested.py
+++ nested.py
@@ -1,5 +1,5 @@
-for x in xrange(1,5 ):
+for x in range(1,5 ):
 ### Inner for loop ###
- for y in xrange(1, 5):
+ for y in range(1, 5):
     print('%d ' % (x))
-     
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
