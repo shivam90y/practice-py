371be47e2f5acf7c0a64454adc99e51b0a88678b
Shivam Yadav <shivamy@coindelta.com>
2019-01-06 22:59:11 +0530

    practice-py

--- list_constructor.py
+++ list_constructor.py
@@ -4,3 +4,4 @@ list2 =list(("bat","ball","temple","bike","motor","laptop"))
 print(list2)
 list3 =list(("ram","shyam","shiv","vishnu","bhole","bajrang","shivam"))
 print(list3)
+print
---
+++ practice4.py
@@ -0,0 +1,6 @@
+# Write a Python program to convert a tuple to a string.
+
+
+tup = ('S', 'h', 'i', 'v', 'a', 'm', ' ', 'y', 'a', 'd', 'a', 'v')
+str =  ''.join(tup)
+print(str)
